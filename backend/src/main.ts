1aea72ce633daf732472689d03da1dd962336277
IMagnatI <a.szlachta03@gmail.com>
2023-07-23 12:26:50 +0200

    init backend

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  // configure swagger
+  const config = new DocumentBuilder()
+      .setTitle('Umbrella Limited test backend')
+      .setDescription('Backend API for fronted recruitment task')
+      .setVersion('1.0')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
+}
+bootstrap();
